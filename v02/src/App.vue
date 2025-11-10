c792e21f6194389675125241975d2565f4fa7b1b
김가은 <gaeunji1@naver.com>
2025-11-10 23:35:03 +0900

    1110 v-if v-show 디렉티브

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -2,7 +2,8 @@
 export default {
   data: function () {
     return {
-      channel: '왕초보 홈페이지 만들기',
+      // channel: '왕초보 홈페이지 만들기',
+      score: 60,
       subscribe: 2677,
     };
   },
@@ -10,11 +11,18 @@ export default {
 </script>
 
 <template>
-  <h1 v-pre>채널명 : {{ channel }}</h1>
+  <!-- directive : <h1 v-pre>채널명 : {{ channel }}</h1>-->
+  <!--
+  <h1 v-if="score >= 90">평점 : A</h1>
+  <h1 v-else-if="score >= 80">평점 : B</h1>
+  <h1 v-else-if="score >= 70">평점 : C</h1>
+  <h1 v-else>평점 : F</h1>
   <h3>구독자 수 : {{ subscribe }}</h3>
   <p>{{ `오늘의 행운 숫자 : ` + Math.ceil(Math.random() * 45) }}</p>
   <h1 v-html="channel"></h1>
-  <h2 v-text="subscribe"></h2>
+  <h2 v-text="subscribe"></h2>-->
+  <h1 v-if="score != 60">이 영역은 v-if에서 처리하고 있습니다</h1>
+  <h1 v-show="score != 60">이 영역은 v-show에서 처리하고 있습니다</h1>
 </template>
 
 <style></style>
