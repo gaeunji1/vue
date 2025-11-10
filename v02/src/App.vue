92d5793f4f112121ca0534a047dbfb78129ea6b3
김가은 <gaeunji1@naver.com>
2025-11-10 23:18:36 +0900

    1110 text interpolation

---
+++ v02/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data: function () {
+    return {
+      channel: '왕초보 홈페이지 만들기',
+      subscribe: 2677,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>채널명 : {{ channel }}</h1>
+  <h3>구독자 수 : {{ subscribe }}</h3>
+  <p>{{ `오늘의 행운 숫자 : ` + Math.ceil(Math.random() * 45) }}</p>
+</template>
+
+<style></style>
