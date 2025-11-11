3adf39767fc71c207cee07f41bbc9da501ff2bf2
김가은 <gaeunji1@naver.com>
2025-11-12 01:11:02 +0900

    1111 마우스 좌표 출력하기

---
+++ v02_05/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      msg: '',
+    };
+  },
+  methods: {
+    mousePos(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+    mouseClick(e) {
+      this.msg += this.x + ',' + this.y + '\n';
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div id="display" v-on:mousemove="mousePos" v-on:click.right.prevent="mouseClick">
+      x: {{ x }}<br />
+      y: {{ y }}
+    </div>
+    <textarea>{{ msg }}</textarea>
+  </div>
+</template>
+
+<style>
+#display {
+  width: 200px;
+  height: 200px;
+  background-color: goldenrod;
+}
+textarea {
+  height: 500px;
+}
+</style>
