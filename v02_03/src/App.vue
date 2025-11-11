c12e709c3e93b49f8f4b63db0d98f2156026724b
김가은 <gaeunji1@naver.com>
2025-11-11 23:17:27 +0900

    1111 methods 옵션, 사용자 함수 / 메소드 만들기

---
+++ v02_03/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+// export default {
+//   data: function () {
+//     return {
+//       userName: '왕초보',
+//     };
+//   },
+//   methods: {
+//     greeting(userName) {
+//       return 'Hi, there ' + userName;
+//     },
+//   },
+// };
+
+export default {
+  data() {
+    return {
+      num: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.num++;
+    },
+    decrement() {
+      this.num--;
+    },
+  },
+};
+</script>
+
+<template>
+  <!--<h1>{{ greeting('TOM') }}</h1>-->
+  <p>{{ num }}</p>
+  <button v-on:click="increment">Increase number</button>
+  <button v-on:click="decrement">Decrease number</button>
+</template>
+
+<style></style>
