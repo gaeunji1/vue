2eac254a82ff9595d92584ad8e649728220b18d4
김가은 <gaeunji1@naver.com>
2025-11-11 21:40:05 +0900

    1111 v-for

---
+++ v02_copy/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      sports: ['BaseBall', 'Football', 'Volleyball', 'Tennis'],
+    };
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="(sport, index) in sports">{{ index + 1 }} {{ sport }}</li>
+  </ul>
+</template>
+
+<style></style>
