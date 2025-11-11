36206f144084802056099bb5983b81cb111f2e2b
김가은 <gaeunji1@naver.com>
2025-11-11 23:33:38 +0900

    1111 modifier 수정자

---
+++ v02_04/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  // methods: {
+  //   createAlert() {
+  //     alert('경고창이 떴습니다.');
+  //   },
+  // },
+  data() {
+    return { kValue: '' };
+  },
+  methods: {
+    getKey(e) {
+      this.kValue = e.key;
+      alert('alt + s 를 누르셨습니다.');
+    },
+  },
+};
+</script>
+
+<template>
+  <!--<button v-on:click.once="createAlert">클릭해보세요</button>-->
+  <input type="text" v-on:keydown.alt.s="getKey" /><br />
+  <span>{{ kValue }}</span>
+</template>
+
+<style></style>
