ec23280a56935c59e2733dea74212c03935dfdc0
김가은 <gaeunji1@naver.com>
2025-11-13 00:25:35 +0900

    1112 dom 요소의 속성을 동적으로 변경하는 v-bind 디렉티브

---
+++ v02_07/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { useTemplateRef } from 'vue';
+
+export default {
+  data() {
+    return {
+      site_name: 'Daum',
+      url: 'https://daum.net',
+      src:"https://i.namu.wiki/i/RYSIeYYRPme_mI-LyZi7UZvQy14vqz2Kbh6gWwwXUDWuwQ82R7vvwwkUlgVu6jZNB11wDTeOFOgAb0VRgEUTB3xXmji574VWPdOHiYGpQ3mc3fQgAgZYAnQrMcStZOjn_jjl5hjbNQLeDwDXjYUI1g.svg"
+    };
+  },
+  methods:{
+    clickAr : function(){
+      this.site_name="Naver",
+      this.url="https://naver.com"
+    }
+  }
+};
+</script>
+
+<template>
+  <a :href="url">{{ site_name }}</a>
+  <img :src></img>
+  <button @click="clickAr">버튼</button>
+</template>
+
+<style></style>
