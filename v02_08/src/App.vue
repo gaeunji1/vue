2abc07430594a7bda39fd1a9fcc0a63b2e11f81a
김가은 <gaeunji1@naver.com>
2025-11-13 00:34:30 +0900

    1112 class 바인딩

---
+++ v02_08/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { useTemplateRef } from 'vue';
+
+export default {
+  data() {
+    return {
+      bgClass: 'bg',
+      erClass: 'text-danger',
+      isActive: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="border" :class="[isActive ? bgClass : '', erClass]">Warning</div>
+</template>
+
+<style>
+.border {
+  border: 2px solid red;
+}
+.bg {
+  background-color: gray;
+}
+.text-danger {
+  color: red;
+}
+</style>
