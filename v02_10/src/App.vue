502ede96ff0b73d1040eaebaf08e7f13566da166
김가은 <gaeunji1@naver.com>
2025-11-13 01:10:57 +0900

    1112 computed 계산된 속성

---
+++ v02_10/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>Full Name : {{ fullName }}</p>
+    <input v-model="fullName" placeholder="Enter full name" />
+
+    firstName:{{ firstName }}<br>
+    lastName:{{ lastName }}
+  </br>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+    };
+  },
+  computed: {
+    fullname: {
+      get() {
+        return this.firstName + ' ' + this.lastName;
+      },
+      set(newValue) {
+        const name = newValue.split(' ');
+        this.firstName = name[0];
+        this.lastName = names[names.length - 1];
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
