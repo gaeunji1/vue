0010e07e29998729020cd796c8d73ede372908cd
김가은 <gaeunji1@naver.com>
2025-11-13 01:00:35 +0900

    1112 style 바인딩

---
+++ v02_09/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      myWidth: 10,
+    };
+  },
+  methods: {
+    incWidth: function () {
+      this.myWidth += 10;
+    },
+    initWidth: function () {
+      this.myWidth = 10;
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="demo"
+    @mouseover="initWidth"
+    :style="{
+      width: myWidth + 'px',
+    }"
+  ></div>
+  <button v-on:click="incWidth">버튼을 클릭해보시오</button>
+</template>
+
+<style>
+.demo {
+  width: 100px;
+  height: 100px;
+  background-color: blueviolet;
+}
+</style>
