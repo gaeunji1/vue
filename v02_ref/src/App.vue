797dbbd617199fd4c4ffa3b4bb758dfb1432e3cd
김가은 <gaeunji1@naver.com>
2025-11-13 12:03:47 +0900

    1113 ref

---
+++ v02_ref/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  methods: {
+    logInput() {
+      console.log(this.$refs.myInput.value);
+      // console.log(document.getElementById("myInput").value);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <input type="text" ref="myInput" />
+    <button @click="logInput">Log</button>
+  </div>
+</template>
+
+<style></style>
