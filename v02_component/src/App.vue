b8b04070e323ec7b82441b531cac97e67b4e7863
김가은 <gaeunji1@naver.com>
2025-11-13 10:42:09 +0900

    1113 component

---
+++ v02_component/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import LocalComponent from "@/components/localChild.vue";
+export default {
+  components: {
+    LocalComponent,
+  },
+};
+</script>
+
+<template>
+  <h1>App.vue</h1>
+  <LocalComponent />
+  <GlobalChild />
+</template>
---
+++ v02_component/src/components/globalChild.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>전역 컴포넌트</h1>
+  <p>전역 컴포넌트입니다</p>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+p {
+  color: orange;
+}
+</style>
